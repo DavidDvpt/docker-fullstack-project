df1802907062a7cfa66642d01a29f753686e0ef1
david mosca <david.mosca69@gmail.com>
2024-07-28 02:38:17 +0200

    update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Arrêter et supprimer les conteneurs existants
-# docker-compose down
+docker-compose down
 
-# docker-compose build --no-cache 2>&1 | tee build.log
+docker-compose build --no-cache 2>&1 | tee build.log
 # Construire les images et démarrer les conteneurs
-# docker-compose up --build -d
+docker-compose up -d
 
 # Copier les node_modules du conteneur vers l'hôte
 docker cp $(docker-compose ps -q client):/app/client/node_modules ./client
 docker cp $(docker-compose ps -q server):/app/server/node_modules ./server
 
 # Redémarrer les conteneurs avec les volumes montés
-# docker-compose down
-# docker-compose up -d
+docker-compose down
+docker-compose up
